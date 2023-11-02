7c4dba72f6956e0718269d11a9c5555c925cf790
Jiahui-Cheng <chengjiahui@berkeley.edu>
2023-11-01 18:05:43 -0700

    11.1

--- lf-3pi/include/I2cCircular1/I2cCircular1.h
+++ lf-3pi/include/I2cCircular1/I2cCircular1.h
@@ -1,8 +1,8 @@
-#ifndef _i2cmaster_main_H
-#define _i2cmaster_main_H
-#ifndef TOP_LEVEL_PREAMBLE_1745701482_H
-#define TOP_LEVEL_PREAMBLE_1745701482_H
-/*Correspondence: Range: [(10, 2), (22, 34)) -> Range: [(0, 0), (12, 34)) (verbatim=true; src=/home/foobar/EZ2C-Fall23/lf-3pi/src/I2cMaster.lf)*/#include <stdio.h>
+#ifndef _i2ccircular1_main_H
+#define _i2ccircular1_main_H
+#ifndef TOP_LEVEL_PREAMBLE_885991756_H
+#define TOP_LEVEL_PREAMBLE_885991756_H
+/*Correspondence: Range: [(10, 2), (20, 34)) -> Range: [(0, 0), (10, 34)) (verbatim=true; src=/home/foobar/EZ2C-Fall23/lf-3pi/src/I2cCircular1.lf)*/#include <stdio.h>
 #include <pico/stdlib.h>
 #include <hardware/gpio.h>
 #include <pico/binary_info.h>
@@ -10,9 +10,7 @@
 #include <pico/i2c_slave.h>
 #include <string.h>
 
-#define ADDR1 0x55
-#define ADDR2 0x56
-#define ADDR3 0x57
+#define ADDR 0x55
 #define PICO_DEFAULT_I2C_SDA_PIN 4
 #define PICO_DEFAULT_I2C_SCL_PIN 5
 #endif
@@ -24,8 +22,9 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-typedef struct i2cmaster_self_t{
+typedef struct i2ccircular1_self_t{
     self_base_t base; // This field is only to be used by the runtime, not the user.
+    bool master;
     int end[0]; // placeholder; MSVC does not compile empty structs
-} i2cmaster_self_t;
+} i2ccircular1_self_t;
 #endif
